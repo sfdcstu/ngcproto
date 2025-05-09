bb0c799b372173afcc6ad6ae988c6c49fba6ab8d
Jeremy Klukan <jklukan@salesforce.com>
2025-05-08 17:03:03 -0700

    feat: add ZIP archives

--- build.js
+++ build.js
@@ -1,3 +1,4 @@
+import archiver from "archiver";
 import {
     readdir,
     statSync,
@@ -7,6 +8,7 @@ import {
     rmSync,
     cpSync,
     writeFileSync,
+    createWriteStream,
 } from "node:fs";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
@@ -18,25 +20,41 @@ const ngcBuildsDir = resolve(__dirname, "./builds/ngc");
 const distDir = resolve(__dirname, "./dist");
 console.log({ ngcBuildsDir, distDir });
 
+const zipDirectory = async (sourceDir, outPath) => {
+    const archive = archiver("zip", { zlib: { level: 9 } });
+    const stream = createWriteStream(outPath);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .directory(sourceDir, false)
+            .on("error", (err) => reject(err))
+            .pipe(stream);
+
+        stream.on("close", () => resolve());
+        archive.finalize();
+    });
+};
+
 const createDefaultManifest = () => ({
     manifest_version: "1.0",
     manifest_publish_date: new Date().toISOString(),
     available_versions: [],
 });
 
-const buildManifest = async () => {
+const buildManifest = async (buildZip = false) => {
     const ngcBuilds = await promisify(readdir)(ngcBuildsDir);
     console.log({ ngcBuilds });
-    const manifest = ngcBuilds.reduce((m, dir) => {
+    let manifest = createDefaultManifest();
+    for (const dir of ngcBuilds) {
         if (!statSync(resolve(ngcBuildsDir, dir)).isDirectory()) {
             console.log(`${dir} is not a directory`);
-            return m;
+            continue;
         }
         // look for and load the BUILDINFO file
         const buildinfoPath = resolve(ngcBuildsDir, dir, "BUILDINFO.json");
         if (!existsSync(buildinfoPath)) {
             console.log(`${dir} does not contain BUILDINFO.json`);
-            return m;
+            continue;
         }
         const yamlFile = readFileSync(buildinfoPath, { encoding: "utf8" });
         try {
@@ -56,40 +74,52 @@ const buildManifest = async () => {
                     console.log(
                         `Could not match regular expression against ${version}`
                     );
-                    return m;
+                    continue;
                 }
                 const versionTag = version.match(/-([-a-zA-Z0-9]+)$/)?.[1];
                 const destVersion = `${majorMinorVersion}${
                     versionTag ? `-${versionTag}` : ""
                 }`;
                 const path = `<root>/releases/ngc/${destVersion}/${entry_point}`;
-                m.available_versions.push({
+
+                // build a ZIP
+                const archive = buildZip ? `agentforce-messaging-release-${destVersion}.zip` : undefined;
+                if (buildZip) {
+                    await zipDirectory(
+                        resolve(ngcBuildsDir, dir),
+                        resolve(
+                            distDir,
+                            archive
+                        )
+                    );
+                }
+
+                manifest.available_versions.push({
                     version: destVersion,
                     full_version: version,
                     minimum_bootstrap_versions,
                     path,
                     css_file,
                     build_date,
+                    ...buildZip && { archive },
                 });
             }
         } catch (ex) {
-            console.error(`Error while reading YAML for ${dir}`, ex, yamlFile);
+            console.error(`Error processing build for ${dir}`, ex, yamlFile);
         }
-
-        return m;
-    }, createDefaultManifest());
+    };
 
     console.log("rebuildManifest", manifest);
 
     return manifest;
 };
 
-const manifest = await buildManifest();
 if (!existsSync(distDir)) {
     mkdirSync(distDir);
 }
 rmSync(distDir, { recursive: true });
 mkdirSync(resolve(distDir, "releases/ngc"), { recursive: true });
+const manifest = await buildManifest(true);
 cpSync(resolve(ngcBuildsDir, "./"), resolve(distDir, "releases/ngc"), {
     recursive: true,
 });
